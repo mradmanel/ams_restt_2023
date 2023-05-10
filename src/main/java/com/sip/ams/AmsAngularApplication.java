8cfb7fe4dd79e06f2a637f8cf0755505834548bd
mradmanel <benmradmanel@gmail.com>
2023-05-11 00:37:15 +0200

    project ams inisialisation

---
+++ src/main/java/com/sip/ams/AmsAngularApplication.java
@@ -0,0 +1,15 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsAngularApplication.class, args);
+	System.out.println("hello angular");
+	
+	}
+
+}
